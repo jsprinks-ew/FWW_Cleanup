cae7247191091cffaf813beb64856cd8c81fff8c
jsprinks-ew <jsprinks@earthwatch.org.uk>
2020-04-15 17:23:29 +0100

    Author added.

--- build-FWW_Monocle_CleanUp-Desktop_Qt_5_14_2_MinGW_64_bit-Debug/ui_fwwdata_cleanup.h
+++ build-FWW_Monocle_CleanUp-Desktop_Qt_5_14_2_MinGW_64_bit-Debug/ui_fwwdata_cleanup.h
@@ -25,6 +25,8 @@ public:
     QLabel *label;
     QLabel *label_2;
     QLabel *label_3;
+    QLabel *label_4;
+    QLabel *label_5;
     QStatusBar *statusbar;
 
     void setupUi(QMainWindow *FWWData_Cleanup)
@@ -32,12 +34,12 @@ public:
         if (FWWData_Cleanup->objectName().isEmpty())
             FWWData_Cleanup->setObjectName(QString::fromUtf8("FWWData_Cleanup"));
         FWWData_Cleanup->resize(800, 600);
-        FWWData_Cleanup->setStyleSheet(QString::fromUtf8("background-color: rgb(255, 255, 255);"));
+        FWWData_Cleanup->setStyleSheet(QString::fromUtf8("background-color: rgb(255, 255, 127);"));
         centralwidget = new QWidget(FWWData_Cleanup);
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
         label = new QLabel(centralwidget);
         label->setObjectName(QString::fromUtf8("label"));
-        label->setGeometry(QRect(100, 10, 591, 101));
+        label->setGeometry(QRect(100, 10, 581, 101));
         label->setStyleSheet(QString::fromUtf8("background-color: rgb(255, 255, 255);"));
         label->setAlignment(Qt::AlignCenter);
         label_2 = new QLabel(centralwidget);
@@ -45,8 +47,21 @@ public:
         label_2->setGeometry(QRect(10, 10, 91, 101));
         label_3 = new QLabel(centralwidget);
         label_3->setObjectName(QString::fromUtf8("label_3"));
-        label_3->setGeometry(QRect(650, 10, 131, 91));
+        label_3->setGeometry(QRect(660, 10, 131, 101));
+        label_4 = new QLabel(centralwidget);
+        label_4->setObjectName(QString::fromUtf8("label_4"));
+        label_4->setGeometry(QRect(0, 0, 781, 111));
+        label_5 = new QLabel(centralwidget);
+        label_5->setObjectName(QString::fromUtf8("label_5"));
+        label_5->setGeometry(QRect(10, 110, 781, 20));
+        label_5->setStyleSheet(QString::fromUtf8("background-color: rgb(255, 255, 255);"));
+        label_5->setAlignment(Qt::AlignCenter);
         FWWData_Cleanup->setCentralWidget(centralwidget);
+        label_4->raise();
+        label->raise();
+        label_2->raise();
+        label_3->raise();
+        label_5->raise();
         statusbar = new QStatusBar(FWWData_Cleanup);
         statusbar->setObjectName(QString::fromUtf8("statusbar"));
         FWWData_Cleanup->setStatusBar(statusbar);
@@ -62,6 +77,8 @@ public:
         label->setText(QCoreApplication::translate("FWWData_Cleanup", "<html><head/><body><p><span style=\" font-size:18pt;\">FreshWater Watch Data Cleanup Utility</span></p></body></html>", nullptr));
         label_2->setText(QCoreApplication::translate("FWWData_Cleanup", "<html><head/><body><p><img src=\":/images/Images/fresh.jpg\"/></p></body></html>", nullptr));
         label_3->setText(QCoreApplication::translate("FWWData_Cleanup", "<html><head/><body><p><img src=\":/images/Images/monocle.png\"/></p></body></html>", nullptr));
+        label_4->setText(QString());
+        label_5->setText(QCoreApplication::translate("FWWData_Cleanup", "Created by James Sprinks, jsprinks@earthwatch.org.uk", nullptr));
     } // retranslateUi
 
 };
